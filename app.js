01b207e80fb8817e5a4ec9cfd6b2d6fecd4de421
Salvador Guerra <00007515@uca.edu.sv>
2024-11-19 22:07:54 -0600

    Add New configuration

--- app.js
+++ app.js
@@ -73,5 +73,5 @@ app.get('/', (req, res) => {
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+  console.log(`Servidor activo  en http://localhost:${PORT}`);
 });
